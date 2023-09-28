72d910f78e08ee7ed85fa7ff6d8c09223b25e794
michael-graute <m.graute@team-braeutigam.de>
2023-09-28 08:20:49 +0200

    added two more tracks to default sequenzer, added some interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,39 @@
 import {ChangeDetectorRef, Component} from '@angular/core';
 import * as Tone from "tone";
+import {OmniOscillatorSynthOptions} from "tone/build/esm/source/oscillator/OscillatorInterface";
+
+export interface Oscillator {
+  type: string;
+  volume: number;
+  detune: number;
+  phase: number;
+  mute?: boolean;
+  onstop?: () => void;
+  envelope: {
+    attack: number;
+    decay: number;
+    sustain: number;
+    release: number;
+  }
+}
+
+export interface Step {
+  duration: string;
+  enabled: boolean;
+}
+
+export interface Track {
+  id: string;
+  pos: number;
+  name: string;
+  colorScheme: string;
+  duration: string;
+  frequency: string;
+  mute: boolean;
+  solo: boolean;
+  oscillator: Oscillator;
+  steps: Step[];
+}
 
 @Component({
   selector: 'ins-root',
@@ -16,12 +50,12 @@ export class AppComponent {
   public playing: boolean = false;
   public synths: Tone.Synth[] = [];
 
-  get tracks(): any[] {
+  get tracks(): Track[] {
     return this.pTracks;
   }
 
-  addTrack(track: any): void {
-    this.synths.push(new Tone.Synth({'oscillator': track.oscillator}).toDestination());
+  addTrack(track: Track): void {
+    this.synths.push(new Tone.Synth({'oscillator': track.oscillator as OmniOscillatorSynthOptions}).toDestination());
     for(let i = 0; i < 16; i++) {
       track.steps[i] = {duration: '8n', enabled: false};
     }
@@ -69,7 +103,7 @@ export class AppComponent {
 
   trackSoloed(solo: boolean): void {
     if(solo) {
-      if (this.tracks.filter(track => track.solo).length === this.tracks.length) {
+      if (this.tracks.filter(track => track.solo).length === this.tracks.length) { //if all tracks are soloed just unmute all
         this.tracks.forEach(track => {
           track.mute = false;
           track.solo = false;
@@ -82,7 +116,7 @@ export class AppComponent {
         });
       }
     } else {
-      if(this.tracks.filter(track => track.solo).length === 0) {
+      if(this.tracks.filter(track => track.solo).length === 0) { //if no track is soloed just unmute all
         this.tracks.forEach(track => {
           track.mute = false;
         });
@@ -126,15 +160,17 @@ export class AppComponent {
     this.pTracks = [];
     [
       {id: 'track1', pos: 0, name: 'Kick', colorScheme: 'orange', frequency: 'C2', type: 'triangle', mute: false, solo: false},
-      {id: 'track2', pos: 1, name: 'Snare', colorScheme: 'yellow', frequency: 'D#2', type: 'square', mute: false, solo: false},
-      {id: 'track3', pos: 2, name: 'HiHat', colorScheme: 'yellow-green', frequency: 'F2', type: 'sawtooth', mute: false, solo: false},
-      {id: 'track4', pos: 3, name: 'LowTom', colorScheme: 'green', frequency: 'G2', type: 'sine', mute: false, solo: false},
-      {id: 'track5', pos: 4, name: 'MidTom', colorScheme: 'green-blue', frequency: 'A#2', type: 'triangle', mute: false, solo: false},
-      {id: 'track6', pos: 5, name: 'MidTom2', colorScheme: 'teal', frequency: 'C3', type: 'square', mute: false, solo: false},
-      {id: 'track7', pos: 6, name: 'HighTom', colorScheme: 'blue', frequency: 'D#3', type: 'sawtooth', mute: false, solo: false},
-      {id: 'track8', pos: 7, name: 'Clap', colorScheme: 'dark-blue', frequency: 'F3', type: 'sine', mute: false, solo: false},
-      {id: 'track9', pos: 8, name: 'Rim', colorScheme: 'violett', frequency: 'G3', type: 'triangle', mute: false, solo: false},
-      {id: 'track10', pos: 9, name: 'Cowbell', colorScheme: 'magenta', frequency: 'A#3', type: 'square', mute: false, solo: false},
+      {id: 'track2', pos: 1, name: 'Snare', colorScheme: 'yellow', frequency: 'C#2', type: 'triangle', mute: false, solo: false},
+      {id: 'track3', pos: 2, name: 'HiHat', colorScheme: 'yellow-green', frequency: 'D2', type: 'triangle', mute: false, solo: false},
+      {id: 'track4', pos: 3, name: 'LowTom', colorScheme: 'green', frequency: 'D#2', type: 'triangle', mute: false, solo: false},
+      {id: 'track5', pos: 4, name: 'MidTom', colorScheme: 'green-blue', frequency: 'E2', type: 'triangle', mute: false, solo: false},
+      {id: 'track6', pos: 5, name: 'MidTom2', colorScheme: 'teal', frequency: 'F2', type: 'triangle', mute: false, solo: false},
+      {id: 'track7', pos: 6, name: 'HighTom', colorScheme: 'blue', frequency: 'F#2', type: 'triangle', mute: false, solo: false},
+      {id: 'track8', pos: 7, name: 'Clap', colorScheme: 'dark-blue', frequency: 'G2', type: 'triangle', mute: false, solo: false},
+      {id: 'track9', pos: 8, name: 'Rim', colorScheme: 'violett', frequency: 'G#2', type: 'triangle', mute: false, solo: false},
+      {id: 'track10', pos: 9, name: 'Cowbell', colorScheme: 'magenta', frequency: 'A2', type: 'triangle', mute: false, solo: false},
+      {id: 'track11', pos: 9, name: 'Crash', colorScheme: 'red', frequency: 'A#2', type: 'triangle', mute: false, solo: false},
+      {id: 'track12', pos: 9, name: 'Ride', colorScheme: 'red2', frequency: 'B2', type: 'triangle', mute: false, solo: false},
     ].forEach(track => {
       this.addTrack({
         id: track.id,
